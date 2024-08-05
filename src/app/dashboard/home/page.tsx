7c7fd16eb3ccae980233eb6fd3b23a29ec6b485e
Say Glenn <glennsay1234@gmail.com>
2024-08-05 16:52:38 +0800

    handles no transactions

--- src/app/dashboard/home/page.tsx
+++ src/app/dashboard/home/page.tsx
@@ -21,31 +21,8 @@ export default async function Page() {
     session.user?.id as string
   );
 
-  // Combine the transactions.
-  const combined_transactions = transactions.reduce<GroupedTransactions>(
-    (acc, transaction) => {
-      const dateKey = transaction.date.slice(0, 7);
-      if (!acc[dateKey]) {
-        acc[dateKey] = [];
-      }
-      acc[dateKey].push(transaction);
-      return acc;
-    },
-    {}
-  );
-
-  // Sort the transactions in each month in descending order.
-  for (const [month, transactions] of Object.entries(combined_transactions)) {
-    combined_transactions[month] = transactions.sort(
-      (a, b) => new Date(a.date).getTime() - new Date(b.date).getTime()
-    );
-  }
-
   const current_month = new Date().toISOString().slice(0, 7);
   const totalDays = new Date(2024, new Date().getMonth() + 1, 0).getDate();
-
-  // Get current month transactions.
-  const current_month_transactions = combined_transactions[current_month];
   const expenseArray: { day: number; expense: string }[] = new Array(totalDays);
   const incomeArray: { day: number; income: string }[] = new Array(totalDays);
 
@@ -54,24 +31,49 @@ export default async function Page() {
     incomeArray[i] = { day: i + 1, income: "0" };
   }
 
-  current_month_transactions.forEach((transaction) => {
-    const date = new Date(transaction.date).getDate();
-    if (transaction.type === "income") {
-      let currAmount = parseFloat(incomeArray[date - 1]["income"]);
-      currAmount += parseFloat(transaction.amount);
-      incomeArray[date - 1]["income"] = currAmount.toFixed(2);
-      return;
+  if (transactions.length > 0) {
+    // Combine the transactions.
+    const combined_transactions = transactions.reduce<GroupedTransactions>(
+      (acc, transaction) => {
+        const dateKey = transaction.date.slice(0, 7);
+        if (!acc[dateKey]) {
+          acc[dateKey] = [];
+        }
+        acc[dateKey].push(transaction);
+        return acc;
+      },
+      {}
+    );
+
+    // Sort the transactions in each month in descending order.
+    for (const [month, transactions] of Object.entries(combined_transactions)) {
+      combined_transactions[month] = transactions.sort(
+        (a, b) => new Date(a.date).getTime() - new Date(b.date).getTime()
+      );
     }
-    let currAmount = parseFloat(expenseArray[date - 1]["expense"]);
-    currAmount += parseFloat(transaction.amount);
-    expenseArray[date - 1]["expense"] = currAmount.toFixed(2);
-  });
+
+    // Get current month transactions.
+    const current_month_transactions = combined_transactions[current_month];
+
+    current_month_transactions.forEach((transaction) => {
+      const date = new Date(transaction.date).getDate();
+      if (transaction.type === "income") {
+        let currAmount = parseFloat(incomeArray[date - 1]["income"]);
+        currAmount += parseFloat(transaction.amount);
+        incomeArray[date - 1]["income"] = currAmount.toFixed(2);
+        return;
+      }
+      let currAmount = parseFloat(expenseArray[date - 1]["expense"]);
+      currAmount += parseFloat(transaction.amount);
+      expenseArray[date - 1]["expense"] = currAmount.toFixed(2);
+    });
+  }
 
   return (
     <>
       <div className="flex flex-col p-8 sm:w-[65%]">
         <DashboardHeader name={session.user?.name as string} />
-        <div className="px-1 overflow-y-scroll h-[610px] md:px-5">
+        <div className="px-1 overflow-y-scroll h-[590px] md:px-5">
           <ExpenseChart
             expenseData={expenseArray}
             month={formatYearMonth(current_month)}
